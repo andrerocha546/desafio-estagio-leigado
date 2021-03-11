76b696be55d65c6857b2408e0a5d0dfca6a55862
André Rocha <andrerocha546@gmail.com>
2021-03-10 22:34:00 -0300

    feat: criando UserService

---
+++ backend/desafio-estagio/src/main/java/br/com/leigado/services/UserService.java
@@ -0,0 +1,18 @@
+package br.com.leigado.services;
+
+import javax.inject.Inject;
+
+import br.com.leigado.entities.User;
+import br.com.leigado.repositories.UserRepository;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+
+public class UserService {
+	
+	@Inject
+	private UserRepository repository;
+	
+	public PanacheQuery<User> findAll() {
+		return repository.findAll();
+	}
+
+}
