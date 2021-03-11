bda97fb4e17144ed74a4323e06619ebed54d193f
André Rocha <andrerocha546@gmail.com>
2021-03-10 21:41:12 -0300

    refactor: usando Repository ao invés de PanacheEntity

--- backend/desafio-estagio/src/main/java/br/com/leigado/entities/User.java
+++ backend/desafio-estagio/src/main/java/br/com/leigado/entities/User.java
@@ -1,5 +1,7 @@
 package br.com.leigado.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,8 +14,9 @@ import br.com.leigado.entities.data.Password;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
 @Entity
-public class User extends PanacheEntityBase {
-
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
---
+++ backend/desafio-estagio/src/main/java/br/com/leigado/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package br.com.leigado.repositories;
+
+import br.com.leigado.entities.User;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public class UserRepository implements PanacheRepository<User> {
+
+}
