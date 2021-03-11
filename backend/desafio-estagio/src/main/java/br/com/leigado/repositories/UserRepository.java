6ae283dc9928fbd211cd91e39c8f90f2eac754b9
André Rocha <andrerocha546@gmail.com>
2021-03-10 22:32:52 -0300

    fix: adicionando anotação @ApplicationScoped

--- backend/desafio-estagio/src/main/java/br/com/leigado/repositories/UserRepository.java
+++ backend/desafio-estagio/src/main/java/br/com/leigado/repositories/UserRepository.java
@@ -1,8 +1,11 @@
 package br.com.leigado.repositories;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import br.com.leigado.entities.User;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
+@ApplicationScoped
 public class UserRepository implements PanacheRepository<User> {
 
 }
